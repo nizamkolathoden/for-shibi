62f7b604cec6ed49952238c9f4acb52ba7265701
stackhubmasc <86549453+stackhubmasc@users.noreply.github.com>
2021-10-02 11:17:01 +0530

    Update fs.js

--- fs.js
+++ fs.js
@@ -3,7 +3,7 @@ const fs = require("fs");
 fs.readFile("dd.json","utf8",(err,data)=>{
 	if(err){
 		
-		console.error(err.message)
+		console.error(err.message);
 
 	}else{
 	jsondata = JSON.parse(data);
